bfc039b9ee749957644ec3918e73cdeef3c1bc7c
Rafael Carvalho <rafaelcarvalhocs2016@gmail.com>
2020-01-14 16:07:40 -0300

    Acrescentar notas

--- src/exercicios/Exercicio1.java
+++ src/exercicios/Exercicio1.java
@@ -12,6 +12,11 @@ public class Exercicio1 {
         double media = (notas[0] + notas[1] + notas[2] + notas[3])/4;
 
         System.out.println(media);
+
+        if(media =<5){
+            System.out.println("reprovado");
+            System.out.println("Suas notas foram" + notas[0] + ";" + notas[1] + ";" + notas[2] + ";" + notas[3]);
+        }
     }
 
 }
