af29416c2760b7ea359f877f382ee5fe2dcbb8a7
Rafael Carvalho <rafaelcarvalhocs2016@gmail.com>
2020-01-14 15:39:11 -0300

    Criar sitema

---
+++ src/exercicios/Exercicio1.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double[] notas = new double[3];
+
+        notas[0] = 0.23;
+        notas[1] = 3.45;
+
+        System.out.println(notas[0]);
+    }
+
+}
