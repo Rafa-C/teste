77b9bb6279a7477e9789d59b62997d9dbbf12165
Rafael Carvalho <rafaelcarvalhocs2016@gmail.com>
2020-01-14 15:53:40 -0300

    Adicionar media

--- src/exercicios/Exercicio1.java
+++ src/exercicios/Exercicio1.java
@@ -2,13 +2,14 @@ package exercicios;
 
 public class Exercicio1 {
     public static void main(String[] args) {
-        double[] notas = new double[3];
+        double[] notas = new double[4];
 
         notas[0] = 0.23;
         notas[1] = 3.45;
         notas[2] = 7.90;
+        notas[3] = 5.90;
 
-        double media = (notas[0] + notas[1])/2;
+        double media = (notas[0] + notas[1] + notas[2] + notas[3])/4;
 
         System.out.println(media);
     }
